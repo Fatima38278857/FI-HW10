4ea7ddd58f76ad1459f127335150fbfecd8d093b
Kommit <aksaghovafatima@gmail.com>
2023-03-05 23:14:41 +0300

    FI--1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      Predicate<Integer> predicate = new Predicate<Integer>() { // Реализация  функционального интерфейса
+          @Override
+          public boolean test(Integer integer) {
+              return integer > 0;
+          }
+      };
+        System.out.println(predicate.test(4));
+
+        Predicate<Integer> predicate1 = x -> x > 0; // Лябда вырожения
+        System.out.println(predicate1.test(-2));
+
     }
 }
+
+
