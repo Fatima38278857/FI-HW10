19b078bf323a006f6538ac81e2f232c4080acde7
Kommit <aksaghovafatima@gmail.com>
2023-03-06 00:51:42 +0300

    FI-4 Доработано.

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,14 @@ public class Main {
         double a = (int) (Math.random() * 100);
         Supplier<Double> supplier = new Supplier<Double>() {
             public Double get() {
-            Double double1 = null;
-            return a;
+                Double double1 = null;
+                return a;
             }
         };
         System.out.println(supplier.get());
-    }
+
+        Supplier<Integer> supplier1 = () -> (int) (Math.random() * 100);
+        System.out.println(supplier1.get());
 }
 
-
+}
